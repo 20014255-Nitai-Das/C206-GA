e330885ec03afbeedc6715202a5e2b13cba1818d
20007603 <20007603@DESKTOP-BNQDNHL>
2021-07-31 17:13:49 +0800

    Classes done by Jolin

---
+++ C206_CaseStudy/src/PromotionOffers.java
@@ -0,0 +1,12 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 20007603, 31 Jul 2021 5:11:14 pm
+ */
+
+public class PromotionOffers {
+
+}
---
+++ C206_CaseStudy/src/Stall.java
@@ -0,0 +1,12 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 20007603, 31 Jul 2021 5:06:38 pm
+ */
+
+public class Stall {
+
+}
