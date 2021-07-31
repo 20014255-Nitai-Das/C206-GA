3b8d8357d693d80f5ba1f64ceae813abb411d25a
20007603 <20007603@DESKTOP-BNQDNHL>
2021-07-31 21:14:05 +0800

    Removed declaration

--- C206_CaseStudy/src/Food.java
+++ C206_CaseStudy/src/Food.java
@@ -1,12 +1,4 @@
 
-/**
- * I declare that this code was written by me.
- * I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism.
- *
- * 20007603, 31 Jul 2021 5:07:11 pm
- */
-
 public class Food {
 
 }
--- C206_CaseStudy/src/PurchaseOrder.java
+++ C206_CaseStudy/src/PurchaseOrder.java
@@ -1,12 +1,4 @@
 
-/**
- * I declare that this code was written by me.
- * I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism.
- *
- * 20007603, 31 Jul 2021 5:12:57 pm
- */
-
 public class PurchaseOrder {
 
 }
