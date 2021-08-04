ee86d80889a3b61d8fd3e919bab662fc3d39062d
20010825 <20010825@LAPTOP-4TFNOH6M>
2021-08-04 18:04:30 +0800

    Done by Crystal

--- C206_CaseStudy/src/Stall.java
+++ C206_CaseStudy/src/Stall.java
@@ -1,4 +1,27 @@
+public class Stall {  // Done by Crystal
+	
+	private String name; 
+	private double sellingPrice; 
+	
+	public Stall(String name, double sellingPrice) {
+		this.name = name;
+		this.sellingPrice = sellingPrice;
+	}
 
-public class Stall {
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSellingPrice() {
+		return sellingPrice;
+	}
+
+	public void setPrice(double sellingPrice) {
+		this.sellingPrice = sellingPrice;
+	}
 }
+
