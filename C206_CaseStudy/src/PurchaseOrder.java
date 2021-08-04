de5216e9dc38f02524858c79af1cafd5b30d8658
work <work@DESKTOP-M6MHK1V>
2021-08-04 17:13:15 +0800

    updated pruchasOrder

--- C206_CaseStudy/src/PurchaseOrder.java
+++ C206_CaseStudy/src/PurchaseOrder.java
@@ -10,6 +10,32 @@
 
 public class PurchaseOrder {
 
+	private int orderNumber;
+	private String orderDate;
+	
+	public PurchaseOrder(int orderNumber, String orderDate) {
+		
+		this.orderNumber = orderNumber;
+		this.orderDate = orderDate;
+	}
+
+	public int getOrderNumber() {
+		return orderNumber;
+	}
+
+	public void setOrderNumber(int orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+
+	public String getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
+	
+	
 	
 	
 }
