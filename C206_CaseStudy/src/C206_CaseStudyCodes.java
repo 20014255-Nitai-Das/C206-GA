7eeedb8636d5fdd884ffae5c6f9d9e0c5c125114
work <work@DESKTOP-M6MHK1V>
2021-08-05 01:47:51 +0800

    updates done on C206_CaseStudyCodes

--- C206_CaseStudy/src/C206_CaseStudyCodes.java
+++ C206_CaseStudy/src/C206_CaseStudyCodes.java
@@ -3,10 +3,44 @@ import java.util.ArrayList;
 public class C206_CaseStudyCodes {
 
 	public static void main(String[] args) {
-	
-		
-		
+
+		ArrayList<PurchaseOrder> purchaseOrderList = new ArrayList<PurchaseOrder>();
+
+		int option = 0;
+
+		while (option != 4) {
+			userMenu();
+			option += Helper.readInt("Enter your choice > ");
+		}
+
+		if (option == 1) {
+			CanAdmMenu();
+		}
+		else if (option == 2) {
+
+		} 
+		else if (option == 3) {
+
+		} 
+		else if (option == 4) {
+			System.out.println("Goodbye!");
+		}
 	}
-			
-	
+
+	public static void userMenu() {
+		Helper.line(50, "=");
+		System.out.println("Who is using?");
+		System.out.println("1. Canteen Administrator");
+		System.out.println("2. Stall staff ");
+		System.out.println("3. Customer");
+		System.out.println("4. Exit");
+		Helper.line(50, "=");
+
+	}
+	public static void CanAdmMenu() {
+		Helper.line(50,"-");
+		System.out.println("Canteen Administrator Menu");
+		Helper.line(50,"-");
+	}
+
 }
