0d7b733084a79b53db02c02c5e4ac7b155092345
work <work@DESKTOP-M6MHK1V>
2021-08-04 17:51:37 +0800

    updated the main class

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,21 +1,48 @@
 
 public class C206_CaseStudy {
 
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	 int options = 0;
-	 
-	 while(options != 5) {
-		 
-	 }
-		
-		
-	}
-	private void menu() { 
+		int options = 0;
 
-		String title = " Ca ";
-		
+		while (options != 5) {
+			menu();
+			options += Helper.readInt("Enter your option > ");
+			
+			if (options == 1){
+				
+			}
+		}
+	}
+
+	private static void menu() {
+		Helper.line(50, "-");
+		System.out.println("Canteen Automation System");
+		Helper.line(50, "-");
+		System.out.println("1. View");
+		System.out.println("2. Add");
+		System.out.println("3. Delete");
+		System.out.println("4. Quit");
+		Helper.line(50, "-");
+
+	}
+
+	private static void viewMenu() {
+		Helper.line(50, "-");
+		System.out.println("View Menu");
+		Helper.line(50, "-");
+		System.out.println("1. Food");
+		System.out.println("2. Purchase Order");
+		System.out.println("3. ");
+		System.out.println("4. Quit");
+	}
+
+	private static void addMenu() {
+
+	}
+
+	private static void deleteMenu() {
+
 	}
 
 }
