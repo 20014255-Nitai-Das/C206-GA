c0520af0a701e17a76025f09439eaad0b05f3da8
work <work@DESKTOP-M6MHK1V>
2021-08-05 02:19:05 +0800

    Updated the main class

--- C206_CaseStudy/src/C206_CaseStudyCodes.java
+++ C206_CaseStudy/src/C206_CaseStudyCodes.java
@@ -9,25 +9,43 @@ public class C206_CaseStudyCodes {
 		int option = 0;
 
 		while (option != 4) {
-			userMenu();
-			option += Helper.readInt("Enter your choice > ");
-		}
+			C206_CaseStudyCodes.userMenu();
+			option = Helper.readInt("Enter your choice > ");
+		
 
 		if (option == 1) {
 			CanAdmMenu();
-		}
-		else if (option == 2) {
+		} if (option == 2) {
+			
+			int ssOption = 0;
 
-		} 
-		else if (option == 3) {
+			while (ssOption != 5) {
+				StallStaffMenu();
+				ssOption = Helper.readInt("Enter option > ");
+			}
+			if (ssOption == 1) {
 
-		} 
-		else if (option == 4) {
+			}
+			 if (ssOption == 2) {
+
+			}
+			if (ssOption == 3) {
+
+			}
+			if (ssOption == 4) {
+
+			}
+
+		} else if (option == 3) {
+			CustomerMenu();
+		} else if (option == 4) {
 			System.out.println("Goodbye!");
 		}
+}
 	}
 
 	public static void userMenu() {
+		
 		Helper.line(50, "=");
 		System.out.println("Who is using?");
 		System.out.println("1. Canteen Administrator");
@@ -37,10 +55,34 @@ public class C206_CaseStudyCodes {
 		Helper.line(50, "=");
 
 	}
+
 	public static void CanAdmMenu() {
-		Helper.line(50,"-");
+		Helper.line(50, "-");
 		System.out.println("Canteen Administrator Menu");
-		Helper.line(50,"-");
+		Helper.line(50, "-");
+
 	}
 
+	public static void StallStaffMenu() {
+		Helper.line(50, "-");
+		System.out.println("Staff Stall  Menu");
+		Helper.line(50, "-");
+		System.out.println("1. Add Purchase Order");
+		System.out.println("2. View Purchase Order");
+		System.out.println("3. Change Purchase Order");
+		System.out.println("4. Delete Purchase Order");
+	}
+
+	public static void CustomerMenu() {
+		Helper.line(50, "-");
+		System.out.println("Customer Menu");
+		Helper.line(50, "-");
+
+	}
+	public static void addPurchaseOrder() {
+		Helper.line(50, "-");
+		System.out.println("Add Pruchase Order");
+		Helper.line(50, "-");
+	}
+	
 }
