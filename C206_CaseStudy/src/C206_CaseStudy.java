50283d18a7d42145577b565ca132877b644bc100
work <work@DESKTOP-M6MHK1V>
2021-08-04 17:18:10 +0800

    updated main class

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -4,12 +4,17 @@ public class C206_CaseStudy {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	 int options = 0;
+	 
+	 while(options != 5) {
+		 
+	 }
+		
 		
 	}
 	private void menu() { 
 
-		String title = "  ";
+		String title = " Ca ";
 		
 	}
 
